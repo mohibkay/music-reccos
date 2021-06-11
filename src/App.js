0e11c5da9b240189190e26748f235b9be0d55045
Mohib Khan <mohibk.in@gmail.com>
2021-06-11 12:39:21 +0530

    Added styles and youtube link feature

--- src/App.js
+++ src/App.js
@@ -1,30 +1,102 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { ImYoutube2 } from "react-icons/im";
 
 function App() {
   const [genre, setGenre] = useState("Bollywood");
+  const [selectedSong, setSelectedSong] = useState({});
 
   const musicLibrary = {
-    Western: ["Perfect", "Love Me Like You Do"],
-    Bollywood: ["Khairiyat", "Teri Jhuki Nazar"],
-    Bengali: ["Bojhena Sey Bojhena", "Ekla Cholo Re"],
+    Western: [
+      {
+        song: "Perfect",
+        linkPreview: "youtube/perfect",
+        link: "https://www.youtube.com/2Vv-BfVoq4g",
+      },
+      {
+        song: "Love Me Like You Do",
+        linkPreview: "youtube/Love Me Like You Do",
+        link: "https://www.youtube.com/AJtDXIazrMo",
+      },
+      {
+        song: "What About Us",
+        linkPreview: "youtube/What About Us",
+        link: "https://www.youtube.com/ClU3fctbGls",
+      },
+    ],
+    Bollywood: [
+      {
+        song: "Khairiyat",
+        linkPreview: "youtube/Khairiyat",
+        link: "https://youtu.be/hoNb6HuNmU0",
+      },
+      {
+        song: "Teri Jhuki Nazar",
+        linkPreview: "youtube/Teri Jhuki Nazar",
+        link: "https://youtu.be/ZgIzch1Pqj4",
+      },
+      {
+        song: "Tu Chahiye",
+        linkPreview: "youtube/Tu Chahiye",
+        link: "https://www.youtube.com/zuvla6ABKbs",
+      },
+    ],
+    Bengali: [
+      {
+        song: "Bojhena Sey Bojhena",
+        linkPreview: "youtube/Bojhena Sey Bojhena",
+        link: "https://www.youtube.com/J2JQQm1h6xQ",
+      },
+      {
+        song: "Ekla Cholo Re",
+        linkPreview: "youtube/Ekla Cholo Re",
+        link: "https://www.youtube.com/r94gSTyH2cM",
+      },
+    ],
   };
 
   const genreArray = Object.keys(musicLibrary);
 
+  console.log(selectedSong);
+
   return (
     <div className="App">
-      {genreArray.map((item) => (
-        <button key={item} onClick={() => setGenre(item)}>
-          {item}
-        </button>
-      ))}
+      <div className="container">
+        <h2>Music Reccos</h2>
+        <div className="buttons">
+          {genreArray.map((item) => (
+            <button
+              className={`button ${genre === item && "selected"}`}
+              key={item}
+              onClick={() => setGenre(item)}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
 
-      <ul>
-        {musicLibrary[genre]?.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+        <ul>
+          {musicLibrary[genre]?.map((item) => (
+            <li
+              onClick={() => setSelectedSong(item)}
+              className={`${selectedSong.song === item.song && "song"}`}
+              key={item.song}
+            >
+              {item.song}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="yt-link">
+        <ImYoutube2 className="yt-icon" />
+
+        {selectedSong?.link ? (
+          <a href={selectedSong.link}>{selectedSong.linkPreview}</a>
+        ) : (
+          <p>Click on a song to get YouTube Link</p>
+        )}
+      </div>
     </div>
   );
 }
