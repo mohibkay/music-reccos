492495de13df4e931d0820ddaef0a374532ca4e5
Mohib Khan <mohibk.in@gmail.com>
2021-09-22 17:21:14 +0530

    feat: added rating & desc

--- src/App.js
+++ src/App.js
@@ -10,47 +10,70 @@ function App() {
     Western: [
       {
         song: "Perfect",
+        rating: "⭐⭐⭐⭐⭐",
         linkPreview: "youtube/perfect",
         link: "https://youtu.be/2Vv-BfVoq4g",
+        description: "An Ed Sheeran song",
       },
       {
         song: "Love Me Like You Do",
+        rating: "⭐⭐⭐⭐⭐",
         linkPreview: "youtube/Love Me Like You Do",
         link: "https://youtu.be/AJtDXIazrMo",
+        description: "An Ellie Goldie song",
       },
       {
         song: "What About Us",
+        rating: "⭐⭐⭐⭐⭐",
         linkPreview: "youtube/What About Us",
         link: "https://youtu.be/ClU3fctbGls",
+        description: "A Pink Floyd song",
       },
     ],
     Bollywood: [
       {
         song: "Khairiyat",
+        rating: "⭐⭐⭐⭐⭐",
         linkPreview: "youtube/Khairiyat",
         link: "https://youtu.be/hoNb6HuNmU0",
+        description: "An Arijit Singh song",
       },
       {
         song: "Teri Jhuki Nazar",
+        rating: "⭐⭐⭐⭐⭐",
         linkPreview: "youtube/Teri Jhuki Nazar",
         link: "https://youtu.be/ZgIzch1Pqj4",
+        description: "A song by Shafqat Amanat Ali",
       },
       {
         song: "Tu Chahiye",
+        rating: "⭐⭐⭐⭐⭐",
         linkPreview: "youtube/Tu Chahiye",
         link: "https://youtu.be/zuvla6ABKbs",
+        description: "A song by Javed Ali",
       },
     ],
     Bengali: [
       {
         song: "Bojhena Sey Bojhena",
+        rating: "⭐⭐⭐⭐⭐",
         linkPreview: "youtube/Bojhena Sey Bojhena",
         link: "https://youtu.be/J2JQQm1h6xQ",
+        description: "A song by Arijit Singh",
+      },
+      {
+        song: "Na Re Na",
+        rating: "⭐⭐⭐⭐⭐",
+        linkPreview: "youtube/Na Re Na",
+        link: "https://youtu.be/azLiK9wdxl8",
+        description: "An Arijit Singh song",
       },
       {
         song: "Ekla Cholo Re",
+        rating: "⭐⭐⭐⭐⭐",
         linkPreview: "youtube/Ekla Cholo Re",
         link: "https://youtu.be/r94gSTyH2cM",
+        description: "A song by Amitabh Bachchan",
       },
     ],
   };
@@ -77,13 +100,23 @@ function App() {
 
         <ul>
           {musicLibrary[genre]?.map((item) => (
-            <li
-              onClick={() => setSelectedSong(item)}
-              className={`${selectedSong.song === item.song && "song"}`}
-              key={item.song}
-            >
-              {item.song}
-            </li>
+            <div className="listItems">
+              <li
+                onClick={() => setSelectedSong(item)}
+                className={`${selectedSong.song === item.song && "song"}`}
+                key={item.song}
+              >
+                <span>
+                  {item.song}
+                  <br />
+                  <p style={{ fontSize: "12px", margin: 0, marginTop: "5px" }}>
+                    {item.description}
+                  </p>
+                </span>
+
+                <div>{item.rating}</div>
+              </li>
+            </div>
           ))}
         </ul>
       </div>
